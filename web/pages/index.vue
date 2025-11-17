2f8be3b565fa8af205108dc245e2885695d94013
ToBinio <tobias.frischmann1@gmail.com>
2025-11-17 11:52:49 +0100

    scaffold web client

---
+++ web/pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+type Station = {
+  id: number;
+  pos_x: number;
+  pos_y: number;
+  types: ("taxi" | "bus" | "underground")[];
+};
+
+let { data: stations } = await useFetch<Station[]>(
+  "http://localhost:8080/stations",
+);
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold">Scotland Yard</h1>
+    <div v-if="stations">
+      <h2 class="text-2xl font-bold">Stations</h2>
+      <div>
+        <div class="flex gap-4" v-for="station in stations">
+          <div>{{ station.id }}</div>
+          <div>{{ station.pos_x }}</div>
+          <div>{{ station.pos_y }}</div>
+          <div>{{ station.types.join(", ") }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
