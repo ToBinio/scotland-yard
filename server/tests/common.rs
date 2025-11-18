3f3017f5f0d0dd7255f2f15014be6653058f421b
ToBinio <tobias.frischmann1@gmail.com>
2025-11-18 09:53:47 +0100

    add create game tests

--- server/tests/common.rs
+++ server/tests/common.rs
@@ -1,7 +1,87 @@
-use axum_test::TestServer;
+use axum_test::{TestServer, TestWebSocket};
+use serde::Deserialize;
+use serde_json::json;
 use server::app;
 
 pub fn test_server() -> TestServer {
     let app = app();
     TestServer::builder().http_transport().build(app).unwrap()
+}
+
+pub async fn get_ws_connection(server: &TestServer) -> TestWebSocket {
+    server
+        .get_websocket("/game/ws")
+        .await
+        .into_websocket()
+        .await
+}
+
+pub async fn send_message(
+    connection: &mut TestWebSocket,
+    name: &str,
+    message: Option<serde_json::Value>,
+) {
+    if let Some(message) = message {
+        connection
+            .send_text(format!("[{}] {}", name, message.to_string()))
+            .await;
+    } else {
+        connection.send_text(format!("[{}]", name)).await;
+    }
+}
+
+pub async fn receive_message<T: serde::de::DeserializeOwned>(
+    connection: &mut TestWebSocket,
+) -> (String, Option<T>) {
+    let message = connection.receive_text().await;
+    let mut split = message.split(" ");
+
+    let name = split.next().unwrap().to_string();
+
+    if let Some(data) = split.next() {
+        let data = serde_json::from_str(data).unwrap();
+        return (name, Some(data));
+    }
+
+    (name, None)
+}
+
+pub async fn assert_receive_message<T: serde::de::DeserializeOwned>(
+    connection: &mut TestWebSocket,
+    name: &str,
+) -> Option<T> {
+    let (received_name, message) = receive_message(connection).await;
+    assert_eq!(received_name, name);
+    message
+}
+
+pub async fn assert_receive_error(connection: &mut TestWebSocket, message: &str) {
+    #[derive(Debug, Deserialize)]
+    struct Error {
+        message: String,
+    }
+
+    let (received_name, response) = receive_message::<Error>(connection).await;
+    assert_eq!(received_name, "error");
+    assert_eq!(response.unwrap().message, message);
+}
+
+pub async fn create_game(socket: &mut TestWebSocket) -> String {
+    send_message(
+        socket,
+        "createGame",
+        Some(json!({
+            "number_of_detecives": 4,
+        })),
+    )
+    .await;
+
+    #[derive(Debug, Deserialize)]
+    struct GameCreated {
+        id: String,
+    }
+
+    let response = assert_receive_message::<GameCreated>(socket, "game").await;
+
+    response.unwrap().id
 }
---
+++ server/tests/create_game.rs
@@ -0,0 +1,87 @@
+use serde::Deserialize;
+use serde_json::json;
+
+use crate::common::{
+    assert_receive_error, assert_receive_message, create_game, get_ws_connection, send_message,
+    test_server,
+};
+
+mod common;
+
+#[tokio::test]
+async fn can_connect() {
+    let server = test_server();
+
+    let response = server.get_websocket("/game/ws").await;
+
+    response.assert_status_switching_protocols();
+}
+
+#[tokio::test]
+async fn can_start_game() {
+    let server = test_server();
+
+    let mut player_1 = get_ws_connection(&server).await;
+    let mut player_2 = get_ws_connection(&server).await;
+
+    let game_id = create_game(&mut player_1).await;
+
+    send_message(&mut player_1, "joinGame", Some(json!({ "id": game_id }))).await;
+    send_message(&mut player_2, "joinGame", Some(json!({ "id": game_id }))).await;
+
+    send_message(&mut player_2, "startGame", None).await;
+
+    #[derive(Debug, Deserialize)]
+    struct GameStarted {
+        role: String,
+    }
+
+    let response = assert_receive_message::<GameStarted>(&mut player_1, "gameStarted").await;
+    let role_1 = response.unwrap().role;
+
+    let response = assert_receive_message::<GameStarted>(&mut player_2, "gameStarted").await;
+    let role_2 = response.unwrap().role;
+
+    assert!(
+        role_1 == "detective" && role_2 == "mister_x"
+            || role_1 == "mister_x" && role_2 == "detective"
+    );
+}
+
+#[tokio::test]
+async fn can_not_join_game_twice() {
+    let server = test_server();
+
+    let mut player = get_ws_connection(&server).await;
+    let game_id = create_game(&mut player).await;
+
+    send_message(&mut player, "joinGame", Some(json!({ "id": game_id }))).await;
+    send_message(&mut player, "joinGame", Some(json!({ "id": game_id }))).await;
+
+    assert_receive_error(&mut player, "game already joined").await;
+}
+
+#[tokio::test]
+async fn can_not_join_unknown_game() {
+    let server = test_server();
+
+    let mut player = get_ws_connection(&server).await;
+
+    send_message(&mut player, "joinGame", Some(json!({ "id": "unknown" }))).await;
+
+    assert_receive_error(&mut player, "game does not exist").await;
+}
+
+#[tokio::test]
+async fn can_not_start_game_without_enough_players() {
+    let server = test_server();
+
+    let mut player = get_ws_connection(&server).await;
+    let game_id = create_game(&mut player).await;
+
+    send_message(&mut player, "joinGame", Some(json!({ "id": game_id }))).await;
+
+    send_message(&mut player, "startGame", None).await;
+
+    assert_receive_error(&mut player, "game does not have enough players").await;
+}
