4ecbed55a4daed6d5a134b3d31be7e0719c81363
tobinio <Tobias.frischmann1@gmail.com>
2025-11-23 12:38:35 +0100

    lint/fmt

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -2,10 +2,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	css: ["~/assets/css/main.css"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
